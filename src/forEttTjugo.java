b9ee250006d0889aeadd23e609cee01be18cf613
Anna-Maja Lithner <am_lithner@hotmail.com>
2017-11-13 14:22:01 +0100

    First commit of v8-exc019-1-1. Contains for1-20.java.

---
+++ src/forEttTjugo.java
@@ -0,0 +1,12 @@
+//package övningsuppgifter;
+/*
+ * skapa ett program som med en for-loop skriver ut siffrorna 1-20.
+ */
+public class forEttTjugo {
+	public static void main (String[] args) {
+		
+		for (int i=1; i<=20; i++) {
+			System.out.println(i);
+		}
+	}
+}
